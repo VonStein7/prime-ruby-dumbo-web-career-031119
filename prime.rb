ee2f7c525784856dea5448c5c860fa4eeebcb36a
brenden williams <brendenw7@gmail.com>
2019-03-10 17:03:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime? (num)
+  if num <= 1 
+    return false
+  end
+  
+  if num <= 3
+    return true
+  end
+  
+  if num % 2 == 0 
+    return false 
+  end
+  
+  divisor = 3
+  while divisor < num 
+    if num % divisor == 0 
+      return false
+    end
+    
+    divisor +=2
+  end
+  puts num < 0 
+  true 
+end
+
