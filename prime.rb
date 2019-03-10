f1f112082fba67b9f876d878dc3f0191e8809265
brenden williams <brendenw7@gmail.com>
2019-03-10 16:38:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime? (num)
+  if num == 2 
+    true 
+  end
+  
+  if num == 3
+    true 
+  end
+  
+  if num % 2 == 0 
+    false 
+  end
+  
+  divisor = 3
+  while divisor < num 
+    if num % divisor == 0 
+      false
+    end
+    
+    divisor +=2  
+  end
+end
